d98a4ecdbb505f62cb51e5627d36f978b11a7cf2
emilselene <118793703+emilselene@users.noreply.github.com>
2022-11-30 02:56:50 -0500

    Add files via upload

---
+++ js/pagina1.js
@@ -0,0 +1,18 @@
+/* Funciones incorporadas */
+
+alert("Bienvenido");
+
+/*
+Datos de entrada
+variables 
+*/
+
+num1=12;
+num2=10;
+
+/* proceso */
+total = num1 + num2;
+
+/* salida */
+
+alert("la suma es: " + total);
